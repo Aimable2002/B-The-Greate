ee958bbd8bff8e3de8a37451560fae66e9fec846
aimable <aganzerizen@gmail.com>
2024-10-27 16:58:02 +0200

    Third Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
@@ -6,12 +6,32 @@ import View from './Pages/View'
 
 function App() {
 
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 700);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 700);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='w-screen'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/view' element={<View />} />
-      </Routes>
+      {isSmallScreen ? (
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/view' element={<View />} />
+        </Routes>
+      ) : (
+        <div className='w-flex items-center justify-center flex-1'>
+          <h1>
+          iPads, PCs, and large TV screens are still in development.
+            Use Smart Phones
+          </h1>
+        </div>
+      )}
     </div>
   )
 }

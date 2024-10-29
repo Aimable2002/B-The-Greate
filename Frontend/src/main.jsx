ffd15f4bf680da3ca4871ae0c75498562585aa75
aimable <aganzerizen@gmail.com>
2024-10-29 18:29:21 +0200

    29 Oct 24 6:29 PM

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,23 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { NextUIProvider } from '@nextui-org/react'
 import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
+// import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}else{
-  console.log('clerk works')
-}
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key")
+// }else{
+//   console.log('clerk works')
+// }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <NextUIProvider>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-          <App />
-        </ClerkProvider>
+        <App />
       </NextUIProvider>
     </BrowserRouter>
   </StrictMode>,

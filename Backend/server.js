e519972c6fa58bacdfbdf0133670a8c03213f1bf
aimable <aganzerizen@gmail.com>
2024-11-09 14:01:20 +0200

    9 Nov 11 2024 PM

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+    origin: '*', // Allow requests from all domains
+    methods: 'GET,POST,PUT,DELETE', // Allow specific methods (adjust if needed)
+    allowedHeaders: 'Content-Type, Authorization' // Allow specific headers
+}));
 
 
 app.use('/api/upload', UploadRoute)
